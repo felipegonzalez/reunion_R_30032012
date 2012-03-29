a418c76a4f2a66f91b62616c415bf70fc9146c85
Felipe Gonzalez <felipexgonzalez@gmail.com>
2012-03-28 18:23:48 -0600

    Script para graficar frecuencias 2011 vs 2012

---
+++ src/1_uso_funciones_L15L.R
@@ -0,0 +1,28 @@
+library(ProjectTemplate)
+load.project()
+
+
+
+colnames(output.2011) <- c('funcion','frec.2011')
+colnames(output.2012) <- c('funcion','frec.2012')
+
+datos <- join(output.2012, output.2011, by="funcion", type='full')
+names(datos) <- c("funcion","frec.2012","frec.2011")
+
+#Calcular frecuencias 2011 y 2012 para las más usadas en 2011
+
+ambas.3 <- arrange(datos, -frec.2011)
+ambas.3$prop.2011 <- ambas.3$frec.2011/sum(ambas.3$frec.2011, na.rm=TRUE)
+ambas.3$prop.2012 <- ambas.3$frec.2012/sum(ambas.3$frec.2012, na.rm=TRUE)
+
+mis.ops <- opts(panel.background = theme_rect(colour = 'gray80')) # or theme_blank()
+
+ggplot(ambas.3[3:100,], aes(x=prop.2011, y=prop.2012,label=funcion,group=1)) + 
+    geom_abline(xintercept=0, slope=1, colour="gray50") + 
+    geom_point(col='salmon') + 
+    scale_x_sqrt() + scale_y_sqrt() +
+    geom_text(size=3.5, hjust= -0.1, colour='gray20') +
+    xlab('Frecuencia 2011') + ylab('Frecuencia 2012') +
+    opts(title="100 funciones más usadas en 2011 (excluyendo function y names)") + mis.ops
+
+
