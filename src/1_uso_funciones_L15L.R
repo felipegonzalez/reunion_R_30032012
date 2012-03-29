7aca734b161035a06629de6e2a5b4361b4a8fc54
Felipe Gonzalez <felipexgonzalez@gmail.com>
2012-03-28 22:33:52 -0600

    Código para producir gráficas y rango-frecuencia

--- src/1_uso_funciones_L15L.R
+++ src/1_uso_funciones_L15L.R
@@ -17,6 +17,7 @@ ambas.3$prop.2012 <- ambas.3$frec.2012/sum(ambas.3$frec.2012, na.rm=TRUE)
 
 mis.ops <- opts(panel.background = theme_rect(colour = 'gray80')) # or theme_blank()
 
+pdf(file='./graps/frecuencias_2011vs2012.pdf')
 ggplot(ambas.3[3:100,], aes(x=prop.2011, y=prop.2012,label=funcion,group=1)) + 
     geom_abline(xintercept=0, slope=1, colour="gray50") + 
     geom_point(col='salmon') + 
@@ -24,5 +25,22 @@ ggplot(ambas.3[3:100,], aes(x=prop.2011, y=prop.2012,label=funcion,group=1)) +
     geom_text(size=3.5, hjust= -0.1, colour='gray20') +
     xlab('Frecuencia 2011') + ylab('Frecuencia 2012') +
     opts(title="100 funciones más usadas en 2011 (excluyendo function y names)") + mis.ops
+dev.off()
+# ================================
+# = Gráficas de rango-frecuencia =
+# ================================
+datos.m <- melt(datos, id.vars = 'funcion')
+datos.m <- ddply(datos.m, c('variable'), function(df){
+    df.1 <- arrange(df, -value)
+    df.1$rank <- 1:nrow(df.1)
+    df.1
+})
+
+pdf(file='./graphs/rango_frecuencia.pdf')
+ggplot(datos.m, aes(x = log(rank), y = log(value), colour = variable)) +
+    geom_point() + mis.ops
+dev.off()
+
+
 
 
--- src/2_nubes_de_palabras.R
+++ src/2_nubes_de_palabras.R
@@ -1,8 +1,11 @@
 library(ProjectTemplate)
 #load.project()
 
+pdf(file='./graph/nube_2011.pdf')
 wordcloud(frecs.2011[1:250,"funcion"], sqrt(frecs.2011[1:250,"frec.2011"]), 
     random.order=FALSE, colors=c('Black','Brown','Orange'), rot.per=0.1)
-    
+
+pdf(file='./graph/nube_2011.pdf')
 wordcloud(frecs.2012[1:250,"funcion"], sqrt(frecs.2012[1:250,"frec.2012"]), 
     random.order=FALSE, colors=c('Black','Brown','Orange'), rot.per=0.1)
+dev.off()
