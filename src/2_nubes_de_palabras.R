b045f7cd347acf21b34529ebe513d56c6af6a5f0
Felipe González <felipexgonzalez@gmail.com>
2012-03-28 18:33:56 -0600

    Update src/2_nubes_de_palabras.R

--- src/2_nubes_de_palabras.R
+++ src/2_nubes_de_palabras.R
@@ -1,5 +1,5 @@
 library(ProjectTemplate)
-load.project()
+#load.project()
 
 wordcloud(frecs.2011[1:250,"funcion"], sqrt(frecs.2011[1:250,"frec.2011"]), 
     random.order=FALSE, colors=c('Black','Brown','Orange'), rot.per=0.1)
